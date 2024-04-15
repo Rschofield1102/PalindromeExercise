eeee77deb84fc5c74a75493cf14fef4923ae1346
Robert Schofield <rschofield1992@gmail.com>
2024-04-15 16:03:13 -0400

    commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise	
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+		public bool IsAPalindrome(string kayak)
+		{
+			var reversed = "";
+			for (int i = kayak.Length - 1; i >=  0; i--)
+			{ reversed += kayak[i]; }
+			if (reversed.ToLower() == kayak.ToLower())
+			{ return true; }
+			else
+			{ return false; }
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,11 +1,28 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("kayaks", false)]// ← you will need to put some test data and expected result here.  
+        public void IsAPalindrome(string x, bool expected)
+        {
+
+            //AAA
+            var test = new WordSmith();
+
+            var actual = test.IsAPalindrome(x);
+            Assert.Equal(expected,actual);
+
+
+        }
+        // For example [InlineData(“racecar”, true)]  
+
+
         public void Test1()
         {
 
